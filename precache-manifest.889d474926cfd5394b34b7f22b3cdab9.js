bc024a86b7cfc890bebf0084125c6ee940d6c9d8
sinu02 <n.srinuee11@gmail.com>
2020-04-30 18:43:57 +0530

    Updates

---
+++ precache-manifest.889d474926cfd5394b34b7f22b3cdab9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34fc4d5d0e61b132b57cb40ac8fe16ef",
+    "url": "/poc-react-app/index.html"
+  },
+  {
+    "revision": "707425e358106c3b711f",
+    "url": "/poc-react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "485e6c2cf6cef5dcb16c",
+    "url": "/poc-react-app/static/js/2.c84153af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poc-react-app/static/js/2.c84153af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "707425e358106c3b711f",
+    "url": "/poc-react-app/static/js/main.95997758.chunk.js"
+  },
+  {
+    "revision": "803f38ace79936d0c105",
+    "url": "/poc-react-app/static/js/runtime-main.58dbdef1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/poc-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
